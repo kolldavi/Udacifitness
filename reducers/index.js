715598e98e86a73e46a2b0e1bb6dc8d4d0bbf3f8
kolldavi <kolldavi@gmail.com>
2017-11-09 16:46:57 -0600

    added reducers

---
+++ reducers/index.js
@@ -0,0 +1,20 @@
+import { RECIEVE_ENTRIES, ADD_ENTRY } from '../actions';
+
+function entries(state = {}, action) {
+  switch (action.type) {
+    case RECIEVE_ENTRIES:
+      return {
+        ...state,
+        ...action.entries
+      };
+    case ADD_ENTRY:
+      return {
+        ...state,
+        ...action.entries
+      };
+    default:
+      return state;
+  }
+}
+
+export default entries;
